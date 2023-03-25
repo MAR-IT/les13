ed32c0dfe7bbabae6f46a5f42064f09238e6c875
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-25 13:55:34 +0100

    kopie

--- script/script13-1.js
+++ script/script13-1.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   $('#slider').slider({
     min: 0,
     max: 100,
-    value: 20
+    value: 50,
+    animate: true,
+    slide: function (event, ui) {
+      $('#divSliderValue').html('Waarde: ' + ui.value);
+    }
   });
 });
